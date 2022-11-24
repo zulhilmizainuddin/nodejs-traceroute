e23447e5ad5f589bbe81066fab68606ea79d62bf
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2022-11-25 05:54:41 +0800

    use module.exports

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import os from 'os';
 import { Tracert } from './tracert';
 import { Traceroute } from './traceroute';
 
-// module.exports = os.platform() === 'win32' ? Tracert : Traceroute;
+module.exports = os.platform() === 'win32' ? Tracert : Traceroute;
 
-const Tracer = os.platform() === 'win32' ? Tracert : Traceroute;
+// const Tracer = os.platform() === 'win32' ? Tracert : Traceroute;
 
-export default Tracer;
+// export default Tracer;
